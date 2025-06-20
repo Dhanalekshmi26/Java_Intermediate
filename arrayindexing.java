6e8e5a2ae028dc9211b37c0ecb3118088c93f7a8
Dhanalekshmi26 <dhanalekshmi2622@gmail.com>
2025-06-20 11:45:56 +0530

    array indexing in java

---
+++ arrayindexing.java
@@ -0,0 +1,12 @@
+public class arrayindexing {
+    public static void main(String[] args){
+        int []myarray = {2,6,8,9,10,3};
+        System.out.println(myarray[2]);
+        System.out.println(myarray[4]);
+        System.out.println(myarray[1]);
+        System.out.println(myarray[3]);
+        System.out.println(myarray[5]);
+        System.out.println(myarray[0]);
+        
+    }
+}
