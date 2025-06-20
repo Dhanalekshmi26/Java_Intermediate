865564d8503230f9dce244826f275f252b7b580b
Dhanalekshmi26 <dhanalekshmi2622@gmail.com>
2025-06-20 11:43:02 +0530

    array looping in java

---
+++ araaylooping.java
@@ -0,0 +1,8 @@
+public class araaylooping {
+    public static void main(String[] args) {
+        int []myarray = {2,6,8,9,10,3};
+        for(int i = 0;i<myarray.length;i++){
+            System.out.println(myarray[i]);
+        }
+    }
+}
