d97387034b837c51b168f9c3a5ba7424ca69b3e8
Dhanalekshmi26 <dhanalekshmi2622@gmail.com>
2025-06-20 11:39:02 +0530

    An another of array in java

---
+++ Arrayexample.java
@@ -0,0 +1,16 @@
+
+import java.util.Arrays;
+
+public class Arrayexample {
+    
+    public static void main(String[] args){
+        int []marks = {90,80,96,85,99,100};
+        System.out.println("overall marks he got-> "+Arrays.toString(marks));
+        String []names = {"jeevan","dwani","dhyan","achu"};
+        System.out.println("babies now: "+Arrays.toString(names));
+
+        
+
+    }
+}
+//Array : its is collection of elements that store in contigeneus memory and can be acess it using index
